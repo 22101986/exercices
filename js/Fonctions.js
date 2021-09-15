d5470e47eb79ef01dee44f463bb2cb647f93d6f8
HaroldPIERRACHE <h.pierrache@gmail.com>
2021-09-15 17:05:14 +0200

    fifth commit

---
+++ js/Fonctions.js
@@ -0,0 +1,10 @@
+// Déclaration de la fonction
+function greetings(firstname, lastname) {
+    return "Bienvenue " + firstname + " " + lastname;
+}
+
+// Appel de la fonction pour exécution
+greetings("Harold", "Pierrache");
+greetings("Hector", "Pierrache");
+var speach = greetings("Suzane", "Pierrache");
+console.log(speach);
